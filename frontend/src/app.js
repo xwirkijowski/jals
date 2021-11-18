eafd153039dc3336aee989d97f5c55480e8f64c1
Sebastian Wirkijowski <sebastian@wirkijowski.dev>
2021-11-19 00:53:30 +0100

    Created Header, Main and Footer components, started using modular scss (css modules)
    Signed-off-by: Sebastian Wirkijowski <sebastian@wirkijowski.dev>

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -9,15 +9,23 @@ import Home from './pages/Home';
 import Info from './pages/Info';
 import Redirect from './pages/Redirect';
 
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Main from './components/Main';
+
 function App() {
 	return (
 		<React.Fragment>
-			<Routes>
-				<Route path="/" element={<Home/>} />
-				<Route exact path="/:_id/" element={<Redirect/>} />
-				<Route exact path="/:_id+/" element={<Info/>} />
-				<Route path="*" element={<Navigate to="/" replace={true} />} />
-			</Routes>
+			<Header/>
+			<Main>
+				<Routes>
+					<Route path="/" element={<Home/>} />
+					<Route exact path="/:_id/" element={<Redirect/>} />
+					<Route exact path="/:_id+/" element={<Info/>} />
+					<Route path="*" element={<Navigate to="/" replace={true} />} />
+				</Routes>
+			</Main>
+			<Footer/>
 		</React.Fragment>
 	);
 }
