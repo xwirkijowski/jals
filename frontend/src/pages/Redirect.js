703b2755b0a76dfed53949f2f4e5cc5f2518e8c5
Sebastian Wirkijowski <sebastian@wirkijowski.dev>
2021-11-19 01:07:01 +0100

    Expanded Redirect view (page) to full basic functionality
    Users will now be redirected to target URL after 5 seconds (only for testing) if short code is valid. If short code invalid users will be redirected to Home view.
    
    Signed-off-by: Sebastian Wirkijowski <sebastian@wirkijowski.dev>

--- frontend/src/pages/Redirect.js
+++ frontend/src/pages/Redirect.js
@@ -1,8 +1,53 @@
 import React from 'react';
+import {
+	Navigate,
+	useParams
+} from "react-router-dom";
+
+import Container from "../components/Container";
+import {gql, useQuery} from "@apollo/client";
+
+// @todo Add createClick mutation
 
 const Redirect = () => {
+	const params = useParams();
+	const isValid = (params._id && (/^[a-f\d]{24}$/i.test(params._id)));
+
+	const query = gql`
+        query GetLink($_id: ID!) {
+            getLink(_id: $_id) {
+                _id
+                target
+                url
+                created
+                clicks
+                flagCount
+            }
+        }
+	`;
+
+	const { loading, error, data } = useQuery(query, {
+		skip: !isValid,
+		variables: {
+			"_id": params._id
+		}
+	});
+
+	if (!isValid) return (<Navigate to="/" replace={true} />);
+
+	if (loading) return <p>Loading...</p>;
+	if (error) return <p>Error! {error}</p>;
+
+	if (!data || !data.getLink) return (<Navigate to="/" replace={true} />);
+
+	setTimeout(() => {
+		window.location.replace(data.getLink.target);
+	}, 5000);
+
 	return (
-		<h2>You will be redirected soon...</h2>
+		<Container>
+			<h2>You will be redirected soon...</h2>
+		</Container>
 	)
 }
 
