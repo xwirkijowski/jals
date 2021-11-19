40b20e328a590022972622888a989e8cf7a85e4d
Sebastian Wirkijowski <sebastian@wirkijowski.dev>
2021-11-19 01:04:25 +0100

    Expanded Home view (page) to full functionality
    Users can now shorten links. Notification containing short code or errors will appear on form submission.
    
    Created the following components:
    - Form
    - InputGroup
    - Input
    
    Signed-off-by: Sebastian Wirkijowski <sebastian@wirkijowski.dev>

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,8 +1,65 @@
 import React from 'react';
+import {
+	gql,
+	useMutation
+} from "@apollo/client";
+
+import Container from "../components/Container";
+import Button from '../components/Button';
+import {
+	Form,
+	InputGroup,
+	Input
+} from '../components/Form';
+import Notification from "../components/Notification";
+import {Link} from "react-router-dom";
+
 
 const Home = () => {
+	const [formState, setFormState] = React.useState({
+		linkToShorten: ''
+	});
+
+	const mutation = gql`
+        mutation addLink($input: LinkInput!) {
+            addLink(input: $input) {
+                link {
+                    _id
+                    target
+                    url
+                    created
+                    clicks
+                }
+            }
+        }
+	`;
+
+	const [addLink, { data, loading, error }] = useMutation(mutation, {
+		variables: {
+			"input": {
+				"target": formState.linkToShorten
+			}
+		}
+	});
+
 	return (
-		<h2>Home</h2>
+		<Container>
+			{ (loading) && <Notification color={"info"}>Shortening in progress...</Notification>}
+			{ (error) && <Notification color={"danger"}>Error! {error.message}</Notification>}
+			{ (data) && <Notification color={"success"}>Link created! Your short code: <Link to={`/${data.addLink.link._id}`}>{data.addLink.link._id}</Link></Notification>}
+			<h2>Paste the URL you want to shorten below</h2>
+			<p className={`${'big'}`}>Your URL will be exchanged for a unique short URL code.</p>
+			<Form onSubmit={e => { e.preventDefault(); addLink() }}>
+				<InputGroup>
+					<Input required type="text" value={formState.url} onChange={(e) => setFormState({ ...formState, linkToShorten: e.target.value }) } placeholder="Your link..." />
+					<Button disabled={(loading)} element="button" type="submit" label="Shorten" inline="true" />
+				</InputGroup>
+			</Form>
+			<p className={`${'disabled'}`}>
+				We count each redirect your short URL has generated.<br/>
+				You can see short URL statistics or target URL by adding a plus sign (+) at the end of your short URL.
+			</p>
+		</Container>
 	)
 }
 
