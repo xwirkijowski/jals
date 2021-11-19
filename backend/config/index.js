b3eaed142b2005bfa0e589b9958caeda93ce3efc
Sebastian Wirkijowski <sebastian@wirkijowski.dev>
2021-11-19 13:27:04 +0100

    Temporary config database string creation added, now requires user and password
    Signed-off-by: Sebastian Wirkijowski <sebastian@wirkijowski.dev>

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,10 +1,13 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+// @todo Remove encodeURIComponent
+// @todo Handle database string creation
+
 export default {
 	port: process.env.PORT,
 	host: process.env.HOST,
-	database: process.env.DATABASE,
+	database: 'mongodb://'+process.env.DATABASE_USER+':'+encodeURIComponent(process.env.DATABASE_PASS)+'@'+process.env.DATABASE,
 	pagination: {
 		perPageDefault: 25,
 		perPageMax: 100
