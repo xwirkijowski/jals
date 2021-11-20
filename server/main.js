b4df3c333f1ea78f5386a258f0b86e01cdd6ab94
Sebastian Wirkijowski <sebastian@wirkijowski.dev>
2021-11-20 03:34:59 +0100

    Remade configuration file, renamed directories to server and client
    Config now handles environment and database connection string construction. Optional logger instances will now not return anything when in production environment and mongoose debug setting will not trigger. Configuration exports config object and env.
    
    Server accepts a full database connection string with MONGOCS or separate variables for database user, password, host, database name and connection options. When separate variables yield MONGOCS. Separate variables are required when using a database user password with characters requiring percentage encoding to avoid errors.
    
    Logger now accepts an additional debug parameter, default false. When set to true, logger displays message only in development mode.
    
    Startup messages moved to config.
    
    Signed-off-by: Sebastian Wirkijowski <sebastian@wirkijowski.dev>

--- server/main.js
+++ server/main.js
@@ -1,4 +1,4 @@
-import config from './config';
+import { config } from './config';
 
 import {ApolloServer} from "apollo-server";
 import mongoose from "mongoose";
@@ -7,34 +7,19 @@ import log from './util/logger';
 import helpers from './util/helpers';
 import { schema, models } from './src';
 
-const about = {
-	version: process.env.npm_package_version,
-	name: process.env.npm_package_name,
-	description: process.env.npm_package_description
-}
-
-// Startup messages
-log.info('---')
-log.info(`Starting ${about.name} v${about.version}`);
-if (about.description) log.info(about.description);
-log.info('---')
-
 log.info('Establishing database connection...')
 mongoose.Promise = global.Promise;
 mongoose.connect(config.database).then(() => {
-	mongoose.set('debug', true); //@todo Remove debug before release
+	mongoose.set('debug', config.debug); //@todo Remove debug before release
 	log.success('Database connection established!')
 
 	// Construct GraphQL server instance
 	const server = new ApolloServer({
 		schema,
 		context: ({}) => {
-			const globals = {
-				host: config.host
-			}
 			const pagination = config.pagination;
 
-			return { globals, pagination, helpers };
+			return { pagination, helpers };
 		},
 		dataSources: () => {
 			return {
