72df308310aad06ed3d392a1a0bcf974782e18d4
Sebastian Wirkijowski <sebastian@wirkijowski.dev>
2024-12-21 01:46:16 +0100

    Added `internalError` and `internalWarning` classes.
    Signed-off-by: Sebastian Wirkijowski <sebastian@wirkijowski.dev>

--- server/src/utilities/internalError.js
+++ server/src/utilities/internalError.js
@@ -1,28 +1,79 @@
 import { globalLogger as log } from './log.js';
 
+// Counter for telemetry
 let counter = 0;
-
+const incrementCounter = () => ++counter;
 export const getCounter = () => counter;
 
-export default class InternalError {
-	message;
+/**
+ * InternalError
+ *
+ * Handles all expected internal errors within the application.
+ *
+ * Logs both critical and non-critical errors.
+ *
+ * @prop	name		Internal name.
+ * @prop	message		The error message.
+ * @prop	domain		The name of the module or domain, used in logs.
+ * @prop	critical	If `true`, throw error;
+ * 						if `silent`, log as critical but do not throw;
+ * 						if `false`, log as error, do not throw.
+ * @prop	payload		Additional payload to be attached;
+ * 						handled internally by Eudoros package on logs.
+ * @prop	stack		Trace to be used instead of default stack.
+ */
+export default class InternalError extends Error {
+	name
+	message
 	domain;
 	critical;
-	arguments;
-	trace;
+	payload;
+	stack;
 
-	constructor(msg, trace, dom, critical = true, ...args) {
-		counter++;
+	/**
+	 * @param message						The error message.
+	 * @param stack							Trace to be used instead of default stack.
+	 * @param domain	{string}			The name of the module or domain, used in logs.
+	 * @param critical	{boolean|'silent'}	If `true`, throw error;
+	 * 										if `silent`, log as critical but do not throw;
+	 * 										if `false`, log as error, do not throw.
+	 * @param payload						Additional payload to be attached;
+	 * 										handled internally by Eudoros package on logs.
+	 */
+	constructor(message, stack = undefined, domain = undefined, critical = true, ...payload) {
+		// Call `Error` constructor
+		super(message.toString())
 
-		[this.message, this.domain, this.critical, this.arguments, this.trace] = [msg, dom, critical, args, trace]
+		// Increment error counter for current error
+		incrementCounter();
 
-		// Log reported error
+		// Assign properties
+		this.name = 'InternalError';
+		this.domain = domain;
+		this.critical = ([true, false, 'silent'].includes(critical)) ? critical : true;
+		this.payload = payload;
+
+		// Prioritize supplied stack on logs to avoid clutter
+		// If stack is not string, convert to string
+		const logTrace =  stack?.toString() || this.stack || undefined;
+		//
+		//	const logTrace =
+		// 		? stack?.toString() || this.stack || undefined
+		// 		: stack?.toString() || undefined;
+
+		// Set the log level
+		const logLevel = this.critical ? 'critical' : 'error';
+
+		// Log error with Eudoros
 		if (this.domain) {
-			log.withDomain(this.critical?'critical':'error', this.domain, this.message||undefined, ...this.arguments,  this.trace||undefined);
+			log.withDomain(logLevel, this.domain, this.message, ...this.payload, logTrace);
 		} else {
-			log[critical?'critical':'error'](this.message, ...this.arguments, this.trace||undefined)
+			log[logLevel](this.message, ...this.payload, logTrace);
 		}
 
-		if (this.critical === true) throw this; // Throw exception if error is critical
+		// @todo Implement Sentry.io
+
+		if (this.critical === true) throw this; // Throw immediately
+		return this; // Allow to handle error by invoker
 	}
 }
--- server/src/utilities/internalWarning.js
+++ server/src/utilities/internalWarning.js
@@ -1,18 +1,56 @@
 import { globalLogger as log } from './log.js';
 
+// Counter for telemetry
 let counter = 0;
-
+const incrementCounter = () => ++counter;
 export const getCounter = () => counter;
 
+/**
+ * InternalWarning
+ *
+ * Handles expected warnings within the application.
+ * Not for error reporting.
+ *
+ * @prop	name		Internal name.
+ * @prop	message		The error message.
+ * @prop	domain		The name of the module or domain, used in logs.
+ * @prop	payload		Additional payload to be attached;
+ * 						handled internally by Eudoros package on logs.
+ * @prop	stack		Trace to be used instead of default stack.
+ */
 export default class InternalWarning {
-	constructor(msg, ext, mod) {
-		counter++;
+	message
+	domain;
+	payload;
+	stack;
+
+	/**
+	 * @param message				The error message.
+	 * @param stack					Trace to be used instead of default stack.
+	 * @param domain	{string}	The name of the module or domain, used in logs.
+	 * @param payload				Additional payload to be attached;
+	 * 								handled internally by Eudoros package on logs.
+	 */
+	constructor(message, stack = undefined, domain = undefined, ...payload) {
+		// Increment error counter for current error
+		incrementCounter();
+
+		// Assign properties
+		this.name = 'InternalWarning';
+		this.message = message.toString();
+		this.domain = domain;
+		this.payload = payload;
+		this.stack = stack?.toString() || undefined;
 
 		// Log reported warning
-		if (mod) {
-			log.withDomain('warn', `${mod}`, msg, ext);
+		if (this.domain) {
+			log.withDomain('warn', `${this.domain}`, this.message, ...this.payload, this.stack);
 		} else {
-			log.warn(msg, ext)
+			log.warn(this.message, ...this.payload, this.stack)
 		}
+
+		// @todo Implement Sentry.io
+
+		return this;
 	}
 }
