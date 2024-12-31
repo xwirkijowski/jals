ea79badce17cd70375dd506823548d210ba0b456
Sebastian Wirkijowski <sebastian@wirkijowski.dev>
2024-12-31 20:29:23 +0100

    Migrated CSS module to Tailwind on spinner component.
    Signed-off-by: Sebastian Wirkijowski <sebastian@wirkijowski.dev>

--- client/src/components/Spinner/Spinner.tsx
+++ client/src/components/Spinner/Spinner.tsx
@@ -1,5 +1,11 @@
-import "./Spinner.module.css";
+import cx from 'classnames';
 
 export const Spinner = () => {
-    return (<div className={'spinner animate-spin font-sm'}></div>)
+    return (
+        <div className={cx(
+            'spinner animate-spin font-sm',
+            'inline-block relative h-[1em] w-[1em] text-[1em] overflow-hidden',
+            'after:content-[""] after:top-0 after:right-0 after:bottom-0 after:left-0 after:absolute after:block after:rounded-full after:border-[0.25em] after:border-transparent after:border-r-[currentColor] after:border-t-[currentColor]'
+        )}></div>
+    )
 }
