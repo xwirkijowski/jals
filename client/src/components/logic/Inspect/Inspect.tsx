37b709618599e2015c53a46857227db4178596a2
Sebastian Wirkijowski <sebastian@wirkijowski.dev>
2024-12-31 03:28:25 +0100

    Added an `Inspect` component, encapsulated inspection input form.
    Signed-off-by: Sebastian Wirkijowski <sebastian@wirkijowski.dev>

---
+++ client/src/components/logic/Inspect/Inspect.tsx
@@ -0,0 +1,55 @@
+'use client'
+
+import {Fragment, useRef} from "react";
+import cx from "classnames";
+import Link from "next/link";
+import {Spinner} from "@comp/Spinner/Spinner";
+import {redirect} from "next/navigation";
+
+export const Inspect = () => {
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    return (
+        <div className={cx('flex w-full flex-col gap-8')}>
+            <form
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    redirect('inspect/'+inputRef?.current?.value, 'push');
+                }}
+
+                className={cx(
+                    'flex group',
+                    'w-full',
+                    'rounded-xl',
+                    'transition-all',
+                    'duration-150',
+                    'shadow-md',
+                    'relative',
+                    'shadow-zinc-900/20',
+                    'focus-within:shadow-lg hover:shadow-lg',
+                )}>
+
+                <input
+                    required
+                    type={"text"}
+                    ref={inputRef}
+                    className={cx(
+                        'flex-1',
+                        'bg-white',
+                        'transition-all duration-150',
+                        'text-zinc-600 text-md caret-orange-500',
+                        'outline-none',
+                        'rounded-xl rounded-r-none',
+                        'border border-transparent border-r-0 px-4 py-2 pr-7 -mr-3',
+                        'placeholder:text-zinc-600/50',
+                        'group-focus-within:border-orange-500',
+                    )} placeholder="Input your URL code"/>
+                <button
+                    type="submit"
+                    className={cx("bg-zinc-900 group-focus-within:bg-orange-500 group-focus-within:hover:bg-orange-400 hover:bg-zinc-700 transition-all duration-150 text-white text-sm font-bold px-5 py-3 rounded-xl flex-0")}>
+                    Inspect
+                </button>
+            </form>
+        </div>
+    )
+}
