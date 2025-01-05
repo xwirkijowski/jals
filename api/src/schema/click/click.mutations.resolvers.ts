385eb56a4fe4d0ecc007970af18d2e6e46b03692
Sebastian Wirkijowski <sebastian@wirkijowski.dev>
2025-01-05 05:06:04 +0100

    Converted click resolvers to TS.
    Signed-off-by: Sebastian Wirkijowski <sebastian@wirkijowski.dev>

--- api/src/schema/click/click.mutations.resolvers.ts
+++ api/src/schema/click/click.mutations.resolvers.ts
@@ -1,20 +1,26 @@
-import {check, getIP, setupMeta} from "../../utilities/helpers.js";
-import {Result} from "../../utilities/result.js";
+import {check, getIP, getUA, setupMeta} from "../../utilities/helpers";
+import {Result} from "../result";
+
+// Types
+import {ContextInterface as CtxI} from "../../types/context.types";
+import {HydratedClick} from "../../types/models/click.types";
+
+// @todo Types
 
 export default {
 	Mutation: {
-		createClick: async (_, {input}, {session, req, models: {click}}) => {
+		createClick: async (_: any, {input}, {session, req, models: {click}}: CtxI) => {
 			check.needs('mongo');
 			const result = new Result();
 
 			// If no User-Agent provided, try request
-			if (!input.userAgent) input.userAgent = req.get('User-Agent').normalize('NFKD') || null;
+			if (!input.userAgent) input.userAgent = getUA(req) || null;
 			// If no IP address provided, try request
 			if (!input.ipAddress) input.ipAddress = getIP(req) || null;
 
 			input = setupMeta(session, input);
 
-			const node = await click.create(input)
+			const node: HydratedClick = await click.create(input)
 
 			if (node?._id) {
 				return result.response(true, {click: node});
@@ -22,7 +28,7 @@ export default {
 				return result.addError('CREATE_CLICK_FAILED').response(true);
 			}
 		},
-		removeClick: async (_, {input}, {session, models: {click}}) => {
+		removeClick: async (_: any, {input}, {session, models: {click}}: CtxI) => {
 			check.needs('mongo');
 			check.isAdmin(session);
 
@@ -36,13 +42,13 @@ export default {
 				return result.addError('REMOVE_CLICK_FAILED').response(true);
 			}
 		},
-		removeAllClicks: async (_, {input}, {session, models: {click, link}}) => {
+		removeAllClicks: async (_: any, {input}, {session, models: {click, link}}: CtxI) => {
 			check.needs('mongo');
 			check.isOwner(session, await link.findOne({_id: input.linkId}));
 
 			const result = new Result();
 
-			const nodeCounts = await click.countDocuments({linkId: input.linkId});
+			const nodeCounts: number = await click.countDocuments({linkId: input.linkId});
 			const node = await click.deleteMany({linkId: input.linkId});
 
 			if (nodeCounts === node.deletedCount) {
--- api/src/schema/click/click.resolvers.ts
+++ api/src/schema/click/click.resolvers.ts
@@ -1,14 +1,19 @@
-import { check, } from "../../utilities/helpers.js";
+import { check } from "../../utilities/helpers";
+
+// Types
+import {ContextInterface as CtxI} from "../../types/context.types";
+
+// @todo Types
 
 export default {
 	Click: {
-		link: async (obj, _, {models: {link}}) => {
+		link: async (obj, _, {models: {link}}: CtxI) => {
 			return (async () => { return await link.findOne({_id: obj.linkId})})();
 		},
-		ipAddress: (obj, _, {session}) => {
+		ipAddress: (obj, _, {session}: CtxI) => {
 			return check.isAdmin(session) ? obj.ipAddress : null;
 		},
-		createdBy: async (obj, _, {session, models: {user}}) => {
+		createdBy: async (obj, _, {session, models: {user}}: CtxI) => {
 			check.isAdmin(session);
 			return (obj.createdBy)
 				? user.findOne({_id: obj.createdBy})
@@ -24,20 +29,20 @@ export default {
 		node: (obj) => { return obj; }
 	},
 	Query: {
-		click: async (_, args, {session, models: {click}}) => {
+		click: async (_: any, args, {session, models: {click}}: CtxI) => {
 			check.isAdmin(session);
 			return await click.findOne({_id: args.clickId});
 		},
-		clicks: async (_, args, {session, models: {click, link}}) => {
-			check.session();
+		clicks: async (_: any, args, {session, models: {click, link}}: CtxI) => {
+			check.session(session);
 			check.isOwner(session, await link.findOne({_id: args.linkId}));
 
 			const clicks = await click.find({linkId: args.linkId})
 
 			return clicks;
 		},
-		countClicks: async (_, args, {session, models: {click, link}}) => {
-			check.session();
+		countClicks: async (_: any, args, {session, models: {click, link}}: CtxI) => {
+			check.session(session);
 			check.isOwner(session, await link.findOne({_id: args.linkId}));
 
 			return await click.countDocuments({linkId: args.linkId});
