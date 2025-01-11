61072cb5c8c5e22d10cfb5daa7104002f79fd0ac
Sebastian Wirkijowski <sebastian@wirkijowski.dev>
2025-01-11 23:57:49 +0100

    Implemented auth directive on user domain.
    Signed-off-by: Sebastian Wirkijowski <sebastian@wirkijowski.dev>

--- api/src/schema/@user/user.resolvers.ts
+++ api/src/schema/@user/user.resolvers.ts
@@ -5,17 +5,10 @@ import {HydratedUser} from "../../types/models/user.types";
 export default {
 	User: {
 		id: ({_id}: {_id: IdType}): IdType => { return _id; },
-		createdBy: async ({createdBy}: {createdBy: IdType}, _: any, {models: {user}}: CtxI): Promise<HydratedUser|null> => {
-			return (createdBy) ? await user.findOne({_id: createdBy}) : null;
-		},
-		updatedBy: async ({updatedBy}: {updatedBy: IdType}, _: any, {models: {user}}: CtxI): Promise<HydratedUser|null> => {
-			return (updatedBy) ? await user.findOne({_id: updatedBy}) : null;
-		},
+		createdBy: async ({createdBy}: {createdBy: IdType}, _: any, {models: {user}}: CtxI): Promise<HydratedUser|null> => (createdBy) ? await user.findOne({_id: createdBy}) : null,
+		updatedBy: async ({updatedBy}: {updatedBy: IdType}, _: any, {models: {user}}: CtxI): Promise<HydratedUser|null> => (updatedBy) ? await user.findOne({_id: updatedBy}) : null,
 	},
 	Query: {
-		user: async (_: any, args, {session, models: {user}}: CtxI): Promise<HydratedUser|null> => {
-			// @todo Auth and `isAdmin` check
-			return await user.findOne({id: args.userId});
-		},
+		user: async (_: any, args, { models: {user}}: CtxI): Promise<HydratedUser|null> => await user.findOne({id: args.userId}),
 	},
 }
