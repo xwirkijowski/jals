f7fc5dbbfd14867db7ad0b3697a39bdd9a45ace8
Sebastian Wirkijowski <sebastian@wirkijowski.dev>
2025-01-15 21:12:21 +0100

    Removed bloat on `+` redirect, implemented redirection mechanism.
    Signed-off-by: Sebastian Wirkijowski <sebastian@wirkijowski.dev>

--- client/src/app/[linkId]/+/page.tsx
+++ client/src/app/[linkId]/+/page.tsx
@@ -1,21 +1,3 @@
-// Metadata
-import {Metadata, ResolvingMetadata} from "next";
-export const generateMetadata = async (
-    { params, searchParams }: Props,
-    parent: ResolvingMetadata
-): Promise<Metadata> => {
-   const linkId = (await params).linkId;
-   return {
-       title: `Inspect ${linkId}`,
-   }
-}
-
-type Props = {
-    params: Promise<{linkId: string}>
-    searchParams: Promise<{[key: string]: string | undefined}>
-}
-
-import cx from 'classnames';
 import {permanentRedirect} from "next/navigation";
 
 const LinkPage = async (
--- client/src/app/[linkId]/page.tsx
+++ client/src/app/[linkId]/page.tsx
@@ -1,18 +1,60 @@
+"use server";
 
 // Metadata
-import { Metadata } from 'next';
-export const metadata: Metadata = {
-    title: 'Redirecting...'
+import {Metadata} from "next";
+export const generateMetadata = async (
+	{ params }
+): Promise<Metadata> => {
+	const linkId = (await params).linkId;
+	return {
+		title: `Redirecting ${linkId}...`,
+	}
 }
 
-const LinkPage = async (
-    {params}
-) => {
-    const linkId = (await params).linkId;
+import { redirect } from "next/navigation";
+import { getClient } from "../../lib/apollo-client";
+import { getSessionContext } from "../../lib/auth/session";
 
-    return (
-        <h1>Redirection... {linkId}</h1>
-    )
+import { gql } from "@apollo/client";
+const LINK = gql`
+    query Link($linkId: ID!) {
+        link(linkId: $linkId) {
+            id
+            target
+            active
+            flagCount
+        }
+    }
+`;
+const CLICK_ADD = gql`
+    mutation ($input: CreateClickInput) {
+        createClick(input: $input) {
+            result {
+                success
+                errors {
+                    path
+                    msg
+                    code
+                }
+            }
+        }
+    }
+`;
+
+export default async function (
+	{params}
+)  {
+	const linkId: string = (await params).linkId;
+
+	const {data: {link}} = await getClient().query({query: LINK, variables: {linkId: linkId}, context: await getSessionContext()});
+	const {data: {createClick: {click}}} = await getClient().mutate({mutation: CLICK_ADD, variables: {input: {linkId: linkId}}, context: await getSessionContext()});
+
+	if (link && link?.active === true && link?.flagCount < 5 && link?.target) {
+		await click;
+		redirect(link.target)
+	}
+
+	return (
+		<h1>Redirecting to {link?.target||linkId}</h1>
+	)
 }
-
-export default LinkPage;
