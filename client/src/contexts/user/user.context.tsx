d9137e83f033da6fa6f0e99cff89317b27d77424
Sebastian Wirkijowski <sebastian@wirkijowski.dev>
2025-01-16 10:36:10 +0100

    Updated `TCurrentUser` type.
    Signed-off-by: Sebastian Wirkijowski <sebastian@wirkijowski.dev>

--- client/src/contexts/user/user.context.tsx
+++ client/src/contexts/user/user.context.tsx
@@ -4,12 +4,10 @@ import {createContext, useContext} from "react";
 import {AuthContext} from "../auth/auth.context";
 
 export type TCurrentUser = {
-	user: {
-		id: string
-		email: string
-		isAdmin: boolean
-		version: number
-	}
+	id: string
+	email: string
+	isAdmin: boolean
+	version: number
 } | null
 
 export const UserContext = createContext<TCurrentUser>(null);
