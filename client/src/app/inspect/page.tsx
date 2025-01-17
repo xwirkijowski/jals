7b312bd522264a5d2147a64c166318318b0a51cf
Sebastian Wirkijowski <sebastian@wirkijowski.dev>
2025-01-17 09:49:36 +0100

    Updated inspect page.
    Signed-off-by: Sebastian Wirkijowski <sebastian@wirkijowski.dev>

--- client/src/app/inspect/page.tsx
+++ client/src/app/inspect/page.tsx
@@ -11,9 +11,9 @@ export default (): React.ReactNode => {
     return (
         <div className="flex flex-col justify-center items-center flex-1 gap-8">
             <div className="text-center">
-                <h2 className="font-bold text-zinc-900 text-2xl/tight sm:text-5xl/tight">
+                <H1>
                     Inspect your <span className={"text-orange-500"}>JALS </span>link
-                </h2>
+                </H1>
             </div>
             <div className="flex flex-col gap-4 justify-center text-center w-full max-w-xl">
                 <InspectForm/>
@@ -21,12 +21,11 @@ export default (): React.ReactNode => {
                     or create a <Link href="/" className="border-b border-b-current text-orange-500 hover:text-orange-400 trans font-bold">new link</Link>
                 </P>
 
-                <div
-                    className="flex flex-col gap-4 text-left w-full max-w-xl mt-8 text-zinc-600 text-md bg-white p-8 shadow-md shadow-zinc-900/20 rounded-xl">
-                    <h3 className={"text-zinc-900 font-bold text-lg/tight sm:text-xl/tight"}>Did you know?</h3>
-                    <p className={""}>You can easily inspect any URL by adding <code
-                        className={'bg-zinc-100 rounded px-1 py-0.5'}>/+</code> at the end.</p>
-                </div>
+                <Card className={'mt-8'}>
+                    <H2 className={'text-base'}>Did you know?</H2>
+                    <P>You can easily inspect any URL by adding <code
+                        className={'bg-zinc-100 dark:bg-gray-900 rounded px-1 py-0.5'}>/+</code> at the end.</P>
+                </Card>
             </div>
         </div>
     )
