c1d42c31ef48b422674b11fc12229a0f1ed1e23e
Sebastian Wirkijowski <sebastian@wirkijowski.dev>
2025-01-18 05:27:47 +0100

    refactor(client): cleaned up authentication actions, implemented typography, card components, updated styling.
    Signed-off-by: Sebastian Wirkijowski <sebastian@wirkijowski.dev>

--- client/src/actions/login/stage1/LogInRequest.form.tsx
+++ client/src/actions/login/stage1/LogInRequest.form.tsx
@@ -1,7 +1,8 @@
 'use client';
 
 import {useRouter} from 'next/navigation';
-import cx from "classnames";
+
+import {TActionProps} from "@act/shared/common.types";
 
 // Components
 import Link from "next/link";
@@ -10,8 +11,12 @@ import {Spinner} from "@comp/Spinner/Spinner";
 import Button from "@comp/Button/Button";
 import {Input} from "@comp/Form/Input";
 import Callout from "@comp/Callout/Callout";
-
-import {TActionProps} from "@act/shared/common.types";
+import {Card} from "@comp/Card/Card";
+import {CardHead} from "@comp/Card/CardHead";
+import {CardBody} from "@comp/Card/CardBody";
+import {CardFooter} from "@comp/Card/CardFooter";
+import {H2} from "@comp/Typography/H2";
+import {P} from "@comp/Typography/P";
 
 export const LogInRequestForm = (
     {mode = 'page', action, state, pending}: TActionProps
@@ -20,43 +25,35 @@ export const LogInRequestForm = (
     const errorCallouts = setupErrorCallouts(state);
 
     return (
-        <form action={action} className="w-full max-w-xl flex flex-col bg-white shadow-xl rounded-xl">
-            <div className={"p-8 flex flex-col gap-4"}>
-                <h2 className="font-bold text-zinc-900 text-2xl/tight sm:text-2xl/tight float-start">
-                    Log in
-                </h2>
-                <p className={cx('text-zinc-600 text-md')}>
-                    If you have an account, you will receive an authentication code needed to log in to your account.
-                </p>
-                <p className={cx('text-zinc-600 text-md')}>You will be able to input the code on the next step. Alternatively you can use the magic link from the email.</p>
-                {errorCallouts}
-            </div>
-            {state?.result?.success ? (
-                <div
-                    className={cx(
-                        'relative p-8 border-y border-zinc-900/15 w-full',
+        <form action={action} className={"w-full max-w-xl"}>
+            <Card structured>
+                <CardHead>
+                    <H2 className="float-start">Log in</H2>
+                    <P>If you have an account, you will receive an authentication code needed to log in to your account.</P>
+                    <P>You will be able to input the code on the next step. Alternatively you can use the magic link from the email.</P>
+                    {errorCallouts}
+                </CardHead>
+                <CardBody>
+                    {state?.result?.success ? (
+                        <Callout type="success">
+                            <p>Authentication code has been send!</p>
+                        </Callout>
+                    ) : (
+                        <Input autoFocus name={'email'} type={'email'} withLabel={'E-mail address'} id={'login-email'} disabled={pending} required/>
                     )}
-                >
-                    <Callout type="success">
-                        <p>Authentication code has been send!</p>
-                    </Callout>
-                </div>
-            ) : (
-                <div className={cx('relative p-8 border-y border-zinc-900/15 flex flex-col w-full gap-4')}>
-                    <Input autoFocus name={'email'} type={'email'} withLabel={'E-mail address'} id={'login-email'} disabled={pending} required/>
-                </div>
-            )}
-            <div className={"col-span-full flex gap-8 p-8 justify-between items-center"}>
-                <Button btnType={'light'} onClick={()=>router.back()}>Close</Button>
-                <div className={'flex gap-4'}>
-                    <Link href={'/register'} passHref replace>
-                        <Button btnType={'light'} disabled={pending} effects={false}>Register</Button>
-                    </Link>
-                    <Button btnType={"primary"} type={'submit'} disabled={pending} effects={true}>
-                        {pending ? (<Spinner/>) : ("Request code")}
-                    </Button>
-                </div>
-            </div>
+                </CardBody>
+                <CardFooter>
+                    <Button btnType={'theme'} onClick={()=>router.back()}>Close</Button>
+                    <div className={'flex gap-4'}>
+                        <Link href={'/register'} passHref replace>
+                            <Button btnType={'theme'} disabled={pending} effects={false}>Register</Button>
+                        </Link>
+                        <Button btnType={"primary"} type={'submit'} disabled={pending} effects={true}>
+                            {pending ? (<Spinner/>) : ("Request code")}
+                        </Button>
+                    </div>
+                </CardFooter>
+            </Card>
         </form>
     )
 }
--- client/src/actions/register/stage1/RegisterRequest.form.tsx
+++ client/src/actions/register/stage1/RegisterRequest.form.tsx
@@ -3,15 +3,21 @@
 import {useRouter} from 'next/navigation';
 import cx from "classnames";
 
-import {TActionProps} from "../../shared/common.types";
+import {TActionProps} from "@act/shared/common.types";
 
 // Components
+import {setupErrorCallouts} from "@act/shared/mutation.utilities";
 import Link from "next/link";
-import {setupErrorCallouts} from "../../shared/mutation.utilities";
 import {Spinner} from "@comp/Spinner/Spinner";
 import Button from "@comp/Button/Button";
 import {Input} from "@comp/Form/Input";
 import Callout from "@comp/Callout/Callout";
+import {Card} from "@comp/Card/Card";
+import {CardHead} from "@comp/Card/CardHead";
+import {CardBody} from "@comp/Card/CardBody";
+import {CardFooter} from "@comp/Card/CardFooter";
+import {H2} from "@comp/Typography/H2";
+import {P} from "@comp/Typography/P";
 
 const list = cx(
     'text-zinc-600 text-md',
@@ -25,49 +31,41 @@ export const RegisterRequestForm = (
     const errorCallouts = setupErrorCallouts(state);
 
     return (
-        <form action={action} className="w-full max-w-xl flex flex-col bg-white shadow-xl rounded-xl">
-            <div className={"p-8 flex flex-col gap-4"}>
-                <h2 className="font-bold text-zinc-900 text-2xl/tight sm:text-2xl/tight float-start">
-                    Create an account
-                </h2>
-                <ul>
-                    <li className={list}>Gain access to detailed statistics</li>
-                    <li className={list}>Access user agent breakdown</li>
-                    <li className={list}>See new clicks as they happen</li>
-                    <li className={list}>Edit your links</li>
-                </ul>
-                <p className={cx('text-zinc-600 text-md')}>
-                    You will receive an authentication code to confirm your email address.
-                </p>
-                <p className={cx('text-zinc-600 text-md')}>You will be able to input the code on the next step. Alternatively you can use the magic link from the email.</p>
-                {errorCallouts}
-            </div>
-            {state?.result?.success ? (
-                <div
-                    className={cx(
-                        'relative p-8 border-y border-zinc-900/15 w-full',
+        <form action={action} className={"w-full max-w-xl"}>
+            <Card structured>
+                <CardHead>
+                    <H2 className="float-start">Create an account</H2>
+                    <ul>
+                        <li className={list}>Gain access to detailed statistics</li>
+                        <li className={list}>Access user agent breakdown</li>
+                        <li className={list}>See new clicks as they happen</li>
+                        <li className={list}>Edit your links</li>
+                    </ul>
+                    <P>You will receive an authentication code to confirm your email address.</P>
+                    <P>You will be able to input the code on the next step. Alternatively you can use the magic link from the email.</P>
+                    {errorCallouts}
+                </CardHead>
+                <CardBody>
+                    {state?.result?.success ? (
+                        <Callout type="success">
+                            <p>Authentication code has been send!</p>
+                        </Callout>
+                    ) : (
+                        <Input autoFocus name={'email'} type={'email'} withLabel={'E-mail address'} id={'register-email'} disabled={pending} required/>
                     )}
-                >
-                    <Callout type="success">
-                        <p>Authentication code has been send!</p>
-                    </Callout>
-                </div>
-            ) : (
-                <div className={cx('relative p-8 border-y border-zinc-900/15 flex flex-col w-full gap-4')}>
-                    <Input autoFocus name={'email'} type={'email'} withLabel={'E-mail address'} id={'register-email'} disabled={pending} required/>
-                </div>
-            )}
-            <div className={"col-span-full flex gap-8 p-8 justify-between items-center"}>
-                <Button btnType={'light'} onClick={()=>router.back()}>Close</Button>
-                <div className={'flex gap-4'}>
-                    <Link href={'/login'} passHref replace>
-                        <Button btnType={'light'} disabled={pending} effects={false}>Log In</Button>
-                    </Link>
-                    <Button btnType={"primary"} type={'submit'} disabled={pending} effects={true}>
-                        {pending ? (<Spinner/>) : ("Request code")}
-                    </Button>
-                </div>
-            </div>
+                </CardBody>
+                <CardFooter>
+                    <Button btnType={'theme'} onClick={()=>router.back()}>Close</Button>
+                    <div className={'flex gap-4'}>
+                        <Link href={'/login'} passHref replace>
+                            <Button btnType={'theme'} disabled={pending} effects={false}>Log In</Button>
+                        </Link>
+                        <Button btnType={"primary"} type={'submit'} disabled={pending} effects={true}>
+                            {pending ? (<Spinner/>) : ("Request code")}
+                        </Button>
+                    </div>
+                </CardFooter>
+            </Card>
         </form>
     )
 }
