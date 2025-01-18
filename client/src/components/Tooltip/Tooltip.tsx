efce561af077f22e4dc53a9d0f19a66ff41560f0
Sebastian Wirkijowski <sebastian@wirkijowski.dev>
2025-01-18 05:23:27 +0100

    chore(client): adjusted tooltip component styling.
    Signed-off-by: Sebastian Wirkijowski <sebastian@wirkijowski.dev>

--- client/src/components/Tooltip/Tooltip.tsx
+++ client/src/components/Tooltip/Tooltip.tsx
@@ -2,6 +2,7 @@ import React, {CSSProperties} from "react";
 import cx from "classnames";
 
 export const Tooltip = ({children, id, style}: { children: React.ReactNode, id: string, style: CSSProperties }) => {
+    //@todo Dark mode
     return (
         <div
             id={id}
@@ -12,6 +13,7 @@ export const Tooltip = ({children, id, style}: { children: React.ReactNode, id: 
                 'text-left',
                 'bottom-[anchor(top)] right-[calc(anchor(right)_-_0.5rem)] [position-area:top_span-left]',
                 'gap-4 flex-col [&:popover-open]:flex max-w-sm overflow-visible rounded-xl bg-white border border-zinc-900/15 text-zinc-900 py-4 px-5 absolute mx-2 my-4 text-sm shadow-xl shadow-zinc-900/10',
+                'dark:shadow-gray-900/10',
                 'before:content-[""] before:block before:absolute before:border-t-8 before:border-t-white before:border-8 before:border-transparent',
                 'before:right-8 before:top-full'
             )}
