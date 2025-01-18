ba3174e0505bcd11bb88e4459626fa7483d14557
Sebastian Wirkijowski <sebastian@wirkijowski.dev>
2025-01-18 05:20:55 +0100

    chore(client): moved and renamed `common.auth.query.ts`.
    Signed-off-by: Sebastian Wirkijowski <sebastian@wirkijowski.dev>

--- client/src/actions/register/stage1/RegisterRequest.action.ts
+++ client/src/actions/register/stage1/RegisterRequest.action.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import {REQUEST_AUTH_CODE} from "../../shared/common.auth.query";
+import {REQUEST_AUTH_CODE} from "../../shared/auth/shared.auth.query";
 import {getClient} from "../../../lib/apollo-client";
 import {ResponseType} from "@type/data/Response";
 import {getSessionHeader} from "../../../lib/auth/session";
