99ba8bff9d643ee9b44aaf71e2fa202ade0a4388
Sebastian Wirkijowski <sebastian@wirkijowski.dev>
2025-01-22 14:13:40 +0100

    refactor(client): adjusted spinner component, now a span.
    Signed-off-by: Sebastian Wirkijowski <sebastian@wirkijowski.dev>

--- client/src/components/Spinner/Spinner.tsx
+++ client/src/components/Spinner/Spinner.tsx
@@ -2,10 +2,10 @@ import cx from 'classnames';
 
 export const Spinner = () => {
     return (
-        <div className={cx(
+        <span className={cx(
             'spinner animate-spin font-sm',
             'inline-block relative h-[1em] w-[1em] text-[1em] overflow-hidden',
             'after:content-[""] after:top-0 after:right-0 after:bottom-0 after:left-0 after:absolute after:block after:rounded-full after:border-[0.25em] after:border-transparent after:border-r-[currentColor] after:border-t-[currentColor]'
-        )}></div>
+        )}></span>
     )
 }
