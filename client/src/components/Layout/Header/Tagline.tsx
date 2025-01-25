d026c3960da3282612bf3dabcb5130893d349c21
Sebastian Wirkijowski <sebastian@wirkijowski.dev>
2025-01-25 23:27:12 +0100

    fix(client): fixed tagline component link.
    Signed-off-by: Sebastian Wirkijowski <sebastian@wirkijowski.dev>

--- client/src/components/Layout/Header/Tagline.tsx
+++ client/src/components/Layout/Header/Tagline.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 export const Tagline = () => {
 	return (
 		<div className="flex flex-row flex-0 items-center gap-4">
-			<h1 className={`text-xl font-bold text-zinc-900 dark:text-gray-200 c-trans-4`}><Link href={'/client/public'}>JALS
+			<h1 className={`text-xl font-bold text-zinc-900 dark:text-gray-200 c-trans-4`}><Link href={'/'}>JALS
 				<span className="text-orange-500">v2</span></Link></h1>
 			<p className={`pointer-events-none hidden text-zinc-600 dark:text-gray-200/50 sm:block c-trans-4`}>Just Another Link Shortener</p>
 		</div>
