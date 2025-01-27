1b70000e90058fa821b7d6d3ab2657c61121f1a3
Sebastian Wirkijowski <sebastian@wirkijowski.dev>
2025-01-27 09:56:28 +0100

    feat(client): added animations to not found organism.
    Signed-off-by: Sebastian Wirkijowski <sebastian@wirkijowski.dev>

--- client/src/components/organisms/NotFound.tsx
+++ client/src/components/organisms/NotFound.tsx
@@ -1,31 +1,35 @@
 "use client";
 
 import cx from "classnames";
-import Link from "next/link";
-import {Button} from "@comp/Button/Button";
 import React from "react";
+import Link from "next/link";
+import {motion} from "motion/react";
+
+import {Button} from "@comp/Button/Button";
 import {H1} from "@comp/Typography/H1";
 import {H2} from "@comp/Typography/H2";
 
+import {container, item} from "@lib/motion/stagger";
+
 export const LinkNotFound = ({linkId, context}): React.ReactNode => {
 	return (
-		<div className={cx('flex-col gap-4 flex items-center max-w-lg w-full')}>
-			<H1 align={"center"}>404</H1>
-			<H2 align={"center"}>We couldn&apos;t find your link</H2>
-			<div className={"flex text-center"}>
+		<motion.div variants={container} className={cx('flex-col gap-4 flex items-center max-w-lg w-full')}>
+			<H1 variants={item} align={"center"}>404</H1>
+			<H2 variants={item} align={"center"}>We couldn&apos;t find your link</H2>
+			<motion.div variants={item} className={"flex text-center"}>
 				<p className={"w-full px-4 py-2 bg-zinc-200 text-zinc-600 rounded-xl border border-transparent font-mono text-wrap break-words overflow-hidden"}>{linkId}</p>
-			</div>
+			</motion.div>
 			{context === 'inspect' && (
-				<div className={'flex gap-4'}>
-					<Link href={`/`} passHref><Button btnType={'dark'} effects={true}>Homepage</Button></Link>
-					<Link href={`/inspect`} passHref><Button btnType={'primary'} effects={true}>Inspect another</Button></Link>
-				</div>
+				<motion.div variants={container} className={'flex gap-4'}>
+					<Link href={`/`} passHref><Button variants={item} btnType={'dark'} effects={true}>Homepage</Button></Link>
+					<Link href={`/inspect`} passHref><Button variants={item} btnType={'primary'} effects={true}>Inspect another</Button></Link>
+				</motion.div>
 			)}
 			{context === 'redirect' && (
-				<div className={'flex gap-4'}>
-					<Link href={`/`} passHref><Button btnType={'primary'} effects={true}>Homepage</Button></Link>
-				</div>
+				<motion.div variants={container} className={'flex gap-4'}>
+					<Link href={`/`} passHref><Button variants={item} btnType={'primary'} effects={true}>Homepage</Button></Link>
+				</motion.div>
 			)}
-		</div>
+		</motion.div>
 	)
 }
