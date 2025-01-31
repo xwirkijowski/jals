62df5df9ab9f3403cf2587ff609dc1e314262d4e
Sebastian Wirkijowski <sebastian@wirkijowski.dev>
2025-01-31 04:42:37 +0100

    feat(client): added back apollo context for client-side queries.
    Signed-off-by: Sebastian Wirkijowski <sebastian@wirkijowski.dev>

---
+++ client/src/contexts/apollo/apollo.context.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import React from "react";
+import {HttpLink} from "@apollo/client";
+import {
+	ApolloNextAppProvider,
+	ApolloClient,
+	InMemoryCache,
+} from "@apollo/experimental-nextjs-app-support";
+
+function makeClient () {
+	const httpLink = new HttpLink({
+		uri: process.env.NEXT_PUBLIC_API_STRING,
+	});
+	
+	const cache = new InMemoryCache({ addTypename: false });
+	
+	return new ApolloClient({
+		cache,
+		link: httpLink,
+		defaultOptions: {
+			query: {
+				errorPolicy: "all"
+			}
+		}
+	});
+}
+
+export function ApolloWrapper ({children}: React.PropsWithChildren) {
+	return (
+		<ApolloNextAppProvider makeClient={makeClient}>
+			{children}
+		</ApolloNextAppProvider>
+	)
+}
