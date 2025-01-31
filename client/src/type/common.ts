692e0be94d496e17dfe751712b721d4269b73896
Sebastian Wirkijowski <sebastian@wirkijowski.dev>
2025-01-31 04:45:46 +0100

    feat(client): added anchor and table cell common types.
    Signed-off-by: Sebastian Wirkijowski <sebastian@wirkijowski.dev>

--- client/src/type/common.ts
+++ client/src/type/common.ts
@@ -11,16 +11,20 @@ export namespace SCompProps {
 	/**
 	 * Local type shorthands
 	 */
+	type Anchor = React.AnchorHTMLAttributes<HTMLAnchorElement>
 	type Button = React.ButtonHTMLAttributes<HTMLButtonElement>
 	type Input = React.InputHTMLAttributes<HTMLInputElement>
 	type Textarea = React.TextareaHTMLAttributes<HTMLTextAreaElement>
 	type Div = React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>
+	type TableCell = React.TdHTMLAttributes<HTMLTableCellElement>
 
 	/**
 	 * Specific HTML elements with optional inline omits
 	 */
+	export type THTMLAnchor<omits extends (keyof Anchor)[]=[]> = Omit<Anchor, omits[number]>;
 	export type THTMLButton<omits extends (keyof Button)[]=[]> = Omit<Button, omits[number]>;
 	export type THTMLInput<omits extends (keyof Input)[]=[]> = Omit<Input, omits[number]>;
 	export type THTMLTextarea<omits extends (keyof Textarea)[]=[]> = Omit<Textarea, omits[number]>;
 	export type THTMLDiv<omits extends (keyof Div)[]=[]> = Omit<Div, omits[number]>
+	export type THTMLTableCell<omits extends (keyof TableCell)[]=[]> = Omit<TableCell, omits[number]>
 }
