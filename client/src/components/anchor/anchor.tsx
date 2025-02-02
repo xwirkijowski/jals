13738638b8089c9b466ee181d59eae32168f9e8d
Sebastian Wirkijowski <sebastian@wirkijowski.dev>
2025-02-02 14:18:31 +0100

    refactor(client): adjusted anchor component, no longer a button.
    Signed-off-by: Sebastian Wirkijowski <sebastian@wirkijowski.dev>

--- client/src/components/anchor/anchor.tsx
+++ client/src/components/anchor/anchor.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React from "react";
-import {motion, MotionProps} from "motion/react";
 import cx from "classnames";
 import Link from "next/link";
 
@@ -10,20 +9,16 @@ import {SCompProps} from "@type/common";
 type TProps = {
 	href: string
 	bold?: boolean
-} & SCompProps.TBase<true> & SCompProps.THTMLButton<["className"]> & MotionProps;
+} & SCompProps.TBase<true> & SCompProps.THTMLAnchor<["className"]>;
 
-export function Anchor ({className, href, bold = false, type = 'button', children, ...props}: TProps): React.ReactNode {
+export function Anchor ({className, href, bold = false, children, ...props}: TProps): React.ReactNode {
 	return (
-		<Link passHref href={href}>
-			<motion.button
-				type={type}
-				className={cx(
-					'text-orange-500 hover:text-orange-400 trans underline !underline-offset-2',
-					{'font-bold': bold},
-					className,
-				)}
-				{...props}
-			>{children}</motion.button>
+		<Link href={href} className={cx(
+			'text-orange-500 hover:text-orange-400 trans underline !underline-offset-2',
+			{'font-bold': bold},
+			className,
+		)} {...props}>
+			{children}
 		</Link>
 	)
 }
