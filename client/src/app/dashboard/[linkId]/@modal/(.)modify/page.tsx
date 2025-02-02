dae0fb650e798521f99fbddd64bac374f1797666
Sebastian Wirkijowski <sebastian@wirkijowski.dev>
2025-02-02 14:58:26 +0100

    feat(client): added link context to dashboard, began work on crud actions.
    Signed-off-by: Sebastian Wirkijowski <sebastian@wirkijowski.dev>

---
+++ client/src/app/dashboard/[linkId]/@modal/(.)modify/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import {useContext} from "react";
+
+import {LinkContext} from "@ctx/link/link.context";
+
+// Components
+
+import {ModalWrapper} from '@comp/modal';
+
+const Page = () => {
+    const {link} = useContext(LinkContext);
+    
+    // @todo Handle broken context edge case
+    
+    if (!link) {return null}
+
+    return (
+        <ModalWrapper>
+            {}
+        </ModalWrapper>
+    )
+}
+
+export default Page;
--- client/src/app/dashboard/layout.tsx
+++ client/src/app/dashboard/layout.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 
 import {ApolloWrapper} from "@ctx/apollo/apollo.context";
+import {LinkContextWrapper} from "@ctx/link/link.context";
 
 export default function Layout ({children}: React.PropsWithChildren): React.ReactNode {
 	return (
 		<ApolloWrapper>
-			{children}
+			<LinkContextWrapper>
+				{children}
+			</LinkContextWrapper>
 		</ApolloWrapper>
 	)
 }
