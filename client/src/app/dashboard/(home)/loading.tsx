9d39ba91c0749d802f81738db2c6eb6e7f9b853e
Sebastian Wirkijowski <sebastian@wirkijowski.dev>
2025-02-02 14:19:38 +0100

    feat(client): implemented new loading page on dashboard home.
    Signed-off-by: Sebastian Wirkijowski <sebastian@wirkijowski.dev>

--- client/src/app/dashboard/(home)/loading.tsx
+++ client/src/app/dashboard/(home)/loading.tsx
@@ -1,5 +1,17 @@
-import {Spinner} from "@comp/spinner";
+import React from "react";
+import cx from "classnames";
+
+import {H1} from "@comp/typography";
+import {LinksTableSkeleton} from "@comp/@dashboard/links-table";
+import {Placeholder} from "@comp/placeholder/placeholder";
 
 export default function Loading () {
-	return (<Spinner />)
+	return (
+		<div className={"w-full flex-col flex gap-8"}>
+			<H1><Placeholder width={'1\/2'} /></H1>
+			<div className={cx("grid grid-cols-4 grid-rows-4 gap-8 flex-1")}>
+				<LinksTableSkeleton/>
+			</div>
+		</div>
+	)
 }
