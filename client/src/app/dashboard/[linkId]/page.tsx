05f39860777f959616369c1e0ff15473bbb2480c
Sebastian Wirkijowski <sebastian@wirkijowski.dev>
2025-02-04 21:01:45 +0100

    feat(client): remade dashboard link inspection page.
    Signed-off-by: Sebastian Wirkijowski <sebastian@wirkijowski.dev>

--- client/src/app/dashboard/[linkId]/page.tsx
+++ client/src/app/dashboard/[linkId]/page.tsx
@@ -1,34 +1,53 @@
 "use client";
 
 import cx from 'classnames';
-import React, {useContext} from "react";
-import Link from "next/link";
+import React, {useContext, useEffect} from "react";
 import {motion} from "motion/react";
-
-// Components
-import {Tooltip} from "@comp/tooltip";
-import {Button} from "@comp/button";
-import {Card, CardBody} from "@comp/card";
-import {H1, H3, P} from "@comp/typography";
-import {Badge} from "@comp/badge";
-import {DateToLocaleString} from "@comp/date-to-locale-string";
-
-// Context
-import {LinkContext} from "@ctx/link/link.context";
+import Link from "next/link";
+import {useRouter} from "next/navigation";
 
 import {container, item} from "@lib/motion/stagger.fly";
 
+// Components
+import {Tooltip} from "@comp/tooltip";
+import {Button, ButtonGroup} from "@comp/button";
+import {Card} from "@comp/card";
+import {H2, P} from "@comp/typography";
+import {Badge} from "@comp/badge";
+import {DateToLocaleString} from "@comp/date-to-locale-string";
+import DataCard from "@comp/@dashboard/data-card/data-card";
+
+// Context
+import {LinkContext} from "@ctx/link/link.context";
+import {AuthContext} from "@ctx/auth/auth.context";
+import {NotificationContext} from "@ctx/notification/notification.context";
+
+
 const Page = (): React.ReactNode => {
     const {link} = useContext(LinkContext);
+    const {user} = useContext(AuthContext);
+    const {add} = useContext(NotificationContext);
+    const router = useRouter();
+    
+    useEffect(() => {
+        if (!link) {
+            add({
+                type: 'danger',
+                title: "Oops!",
+                content: (<p>This link does not exists. Maybe you already deleted it?</p>),
+                dismissible: false,
+            })
+            
+            router.push('/dashboard')
+        }
+    }, []);
 
-    // @todo Handle broken context edge case
-    if (!link) {return null}
-
+    if (!link) return null;
+    
     return (
         <>
             <motion.div className={'flex flex-row gap-4 items-center'} variants={container} >
-                <Link passHref href={'/dashboard'}><Button btnType={'theme'} variants={item}>Go back</Button></Link>
-                <H1 variants={item}>Inspecting <span className={'text-orange-500'}>{link.id}</span></H1>
+                <H2 variants={item}>Inspecting <span className={'text-orange-500'}>{link.id}</span></H2>
                 {/* @ts-ignore workaround for `anchorName` CSS property */}
                 <Badge variants={item} badgeType={link.active?'success':'danger'} tooltip ping style={{anchorName: "--active-popover"}} popoverTarget={'active-popover'} popoverTargetAction={'toggle'}>
                     {link.active ? "Active" : "Not active"}
@@ -42,30 +61,44 @@ const Page = (): React.ReactNode => {
                         flags.</p>
                 </Tooltip>
             </motion.div>
-            <motion.div variants={container} className={cx("grid grid-cols-4 gap-8 flex-1 auto-rows-min")}>
-                <Card variants={container} structured className={'overflow-hidden max-w-none col-span-2'}>
-                    <CardBody variants={container} grid>
-                        <motion.div variants={item} className={"w-full gap-2 flex flex-col col-span-full"}>
-                            <H3 className={"!text-base"}>Target URL</H3>
-                            <p className={"w-full px-4 py-2 bg-zinc-200 text-zinc-600 rounded-xl border border-transparent font-mono text-wrap break-words overflow-hidden"}>{link.target}</p>
-                        </motion.div>
-                        <motion.div variants={item} className={"gap-2 flex flex-col"}>
-                            <H3 className={"!text-base"}>Creation time</H3>
-                            <P><DateToLocaleString value={link.createdAt} /></P>
-                        </motion.div>
-                        <motion.div variants={item} className={"gap-2 flex flex-col"}>
-                            <H3 className={"!text-base"}>Click count</H3>
-                            <P>{link.clickCount > 0 ? link.clickCount : "No clicks yet"}</P>
-                        </motion.div>
-                        <motion.div variants={item} className={"gap-2 flex flex-col"}>
-                            <H3 className={"!text-base"}>Last modified</H3>
-                            <P>{link.version > 0 && link.updatedAt ? <DateToLocaleString value={link.updatedAt} /> : "Not modified yet"}</P>
-                        </motion.div>
-                        <motion.div variants={item} className={"gap-2 flex flex-col"}>
-                            <H3 className={"!text-base"}>Version</H3>
-                            <P>{link.version}</P>
-                        </motion.div>
-                    </CardBody>
+            <motion.div variants={container} className={cx("grid grid-cols-6 gap-8 flex-1 auto-rows-min")}>
+                <DataCard variants={container} label={'Target URL'} className={'col-span-full md:col-span-4'}>
+                    <p className={"w-full px-4 py-2 bg-zinc-200 text-zinc-600 rounded-xl border border-transparent font-mono text-wrap break-words overflow-hidden"}>{link.target}</p>
+                </DataCard>
+                
+                <Card contained={false} structured className={'col-start-5 col-span-2 row-span-full'}>
+                
+                </Card>
+                
+                <DataCard variants={container} label={'Click count'} className={'col-span-full sm:col-span-3 md:col-span-2 lg:col-span-1'}>
+                    <P className={cx(`text-${link.clickCount > 0 ? '4' : ''}xl font-bold`)}>
+                        {link.clickCount > 0 ? link.clickCount : "No clicks"}
+                    </P>
+                </DataCard>
+                
+                <DataCard variants={container} label={'Flag Count'} className={'col-span-full sm:col-span-3 md:col-span-2 lg:col-span-1'}>
+                    <P className={cx(`text-${link.clickCount > 0 ? '4' : ''}xl font-bold`)}>
+                        {link.flagCount || 'No flags'}
+                    </P>
+                </DataCard>
+                
+                <DataCard variants={container} label={'Created at'} className={'col-span-full sm:col-span-3 md:col-span-2'}>
+                    <P className={'text-xl font-bold'}><DateToLocaleString value={link.createdAt}/></P>
+                    <P className={'text-base'}>By {user!.email === link.createdBy.email ? 'you' : link.createdBy.email}</P>
+                </DataCard>
+                
+                <DataCard variants={container} label={'Last modified'} className={'col-span-full sm:col-span-3 md:col-span-2'}>
+                    <P className={'text-xl font-bold'}>{link.version > 0 && link.updatedAt ?
+                        <DateToLocaleString value={link.updatedAt}/> : "Not modified yet"}</P>
+                    <P className={'text-base'}>Version {link.version + 1}</P>
+                </DataCard>
+                
+                <Card variants={container} contained={false} className={"col-span-full flex-row justify-between"}>
+                    <Link passHref href={'/dashboard'}><Button btnType={'theme'} variants={item}>Go back</Button></Link>
+                    <ButtonGroup className={"justify-end"}>
+                        <Link passHref href={`/dashboard/${link.id}/modify`}><Button btnType={'theme'} variants={item}>Modify</Button></Link>
+                        <Link passHref href={`/dashboard/${link.id}/delete`}><Button btnType={'danger'} variants={item}>Delete</Button></Link>
+                    </ButtonGroup>
                 </Card>
             </motion.div>
         </>
