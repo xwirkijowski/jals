6df926a3a93e5fa82fb5d0b6524d853ca8bdfa8a
Sebastian Wirkijowski <sebastian@wirkijowski.dev>
2025-02-05 23:03:24 +0100

    fix(client): forced layout to ball (_as in ballin'_)
    Signed-off-by: Sebastian Wirkijowski <sebastian@wirkijowski.dev>

--- client/src/app/dashboard/layout.tsx
+++ client/src/app/dashboard/layout.tsx
@@ -1,14 +1,14 @@
 "use server";
 
-import React from "react";
+import {ReactNode} from "react";
 import {redirect} from "next/navigation";
 
 import {ApolloWrapper} from "@ctx/apollo/apollo.context";
 import {getSession} from "@ctx/auth/auth.utils.server";
 
-export default async (
-	{modal, children, params}: { modal: React.ReactNode, children: React.ReactNode, params: any }
-): Promise<React.ReactNode> => {
+export default async function Layout (
+	{children}: { children: ReactNode }
+): Promise<ReactNode> {
 	const session = await getSession();
 	
 	if (!session) {
@@ -17,7 +17,6 @@ export default async (
 	
 	return (
 		<ApolloWrapper>
-			{modal}
 			{children}
 		</ApolloWrapper>
 	)
