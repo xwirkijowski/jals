721268d432c02d2e4310ee7b401d775634c91713
Sebastian Wirkijowski <sebastian@wirkijowski.dev>
2025-02-05 22:30:22 +0100

    feat(client): added `contained` property to `Table` component.
    Signed-off-by: Sebastian Wirkijowski <sebastian@wirkijowski.dev>

--- client/src/components/table/table.tsx
+++ client/src/components/table/table.tsx
@@ -7,15 +7,20 @@ import {merge} from "@lib/merge";
 
 import {SCompProps} from "@type/common";
 
-type TProps = SCompProps.TBase<true> & MotionProps;
+type TProps = {
+	contained?: boolean
+} & SCompProps.TBase<true> & MotionProps;
 
-export function Table ({className, children, variants, ...props}: TProps): React.ReactNode {
+export function Table (
+	{contained = false, className, children, variants, ...props}: TProps): React.ReactNode
+{
 	return (
 		<motion.div className={"overflow-auto relative group"} variants={variants}>
 			<motion.table className={merge(
 				"c-trans-4 group-[:not(:first-child)]:first:border-t group-[:not(:last-child)]:border-b padding-t w-full",
 				"border-zinc-200",
 				"dark:border-gray-700",
+				{'table-fixed': contained},
 				className,
 			)} variants={variants} {...props}>
 				{children}
