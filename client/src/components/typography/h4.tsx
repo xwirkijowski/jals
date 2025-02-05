129a518d3fceaa0187f8d063938bba21304f7fdd
Sebastian Wirkijowski <sebastian@wirkijowski.dev>
2025-02-05 22:29:57 +0100

    feat(client): added `H4` component to typography.
    Signed-off-by: Sebastian Wirkijowski <sebastian@wirkijowski.dev>

---
+++ client/src/components/typography/h4.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import React from "react";
+import {motion} from "motion/react";
+
+import {merge} from "@lib/merge";
+
+import {TProps, styles} from "@comp/typography/common";
+
+export const H4 = (
+	{align, className, children, ...props}: TProps
+): React.ReactNode => {
+	return (
+		<motion.h4 className={merge(
+			'font-bold text-base/tight sm:text-base/tight',
+			(align && `text-${align}`),
+			styles,
+			className,
+			'text-zinc-900',
+			'dark:text-zinc-100',
+		)} {...props}>
+			{children}
+		</motion.h4>
+	)
+}
