ce1bb9b2189f3358034792e6759dfc8f80092498
Sebastian Wirkijowski <sebastian@wirkijowski.dev>
2025-02-05 22:36:42 +0100

    refactor(client): adjusted data-card dashboard component.
    Signed-off-by: Sebastian Wirkijowski <sebastian@wirkijowski.dev>

--- client/src/components/@dashboard/data-card/data-card.tsx
+++ client/src/components/@dashboard/data-card/data-card.tsx
@@ -2,13 +2,19 @@
 
 import {motion} from "motion/react";
 
-import {Card} from "@comp/card";
-import {H3} from "@comp/typography";
 import {container, item} from "@lib/motion/stagger.fly";
+import {merge} from "@lib/merge";
 
-export default function DataCard({label, children, ...props}) {
+import {Card, TCardProps} from "@comp/card";
+import {H3} from "@comp/typography";
+
+type TProps = {
+	label: string
+} & TCardProps
+
+export default function DataCard({label, className, children, ...props}: TProps) {
 	return (
-		<Card {...props} contained={false}>
+		<Card {...props} contained={false} className={merge(className, 'justify-between')}>
 			<H3 variants={item}>{label}</H3>
 			<motion.div variants={item} className={"w-full gap-2 flex flex-col"}>
 				{children}
