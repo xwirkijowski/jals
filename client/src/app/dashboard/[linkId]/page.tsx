deb4a60c4e90a52322779f86d939fd3d42da847e
Sebastian Wirkijowski <sebastian@wirkijowski.dev>
2025-02-05 22:38:08 +0100

    feat(client): implemented new components to dashboard link inspection page, reworked its layout.
    Signed-off-by: Sebastian Wirkijowski <sebastian@wirkijowski.dev>

--- client/src/app/dashboard/[linkId]/page.tsx
+++ client/src/app/dashboard/[linkId]/page.tsx
@@ -1,12 +1,18 @@
 "use client";
 
 import cx from 'classnames';
-import React, {useContext, useEffect} from "react";
+import {ReactNode, Suspense, useContext, useEffect} from "react";
 import {motion} from "motion/react";
 import Link from "next/link";
 import {useRouter} from "next/navigation";
 
+import {merge} from "@lib/merge";
 import {container, item} from "@lib/motion/stagger.fly";
+
+// Context
+import {LinkContext} from "@ctx/link/link.context";
+import {AuthContext} from "@ctx/auth/auth.context";
+import {NotificationContext} from "@ctx/notification/notification.context";
 
 // Components
 import {Tooltip} from "@comp/tooltip";
@@ -15,15 +21,11 @@ import {Card} from "@comp/card";
 import {H2, P} from "@comp/typography";
 import {Badge} from "@comp/badge";
 import {DateToLocaleString} from "@comp/date-to-locale-string";
+import {ClickList, ClickListSkeleton} from "@comp/@dashboard/click-list";
+import {FlagList} from "@comp/@dashboard/flag-list";
 import DataCard from "@comp/@dashboard/data-card/data-card";
 
-// Context
-import {LinkContext} from "@ctx/link/link.context";
-import {AuthContext} from "@ctx/auth/auth.context";
-import {NotificationContext} from "@ctx/notification/notification.context";
-
-
-const Page = (): React.ReactNode => {
+export default function Page ():ReactNode {
     const {link} = useContext(LinkContext);
     const {user} = useContext(AuthContext);
     const {add} = useContext(NotificationContext);
@@ -61,39 +63,61 @@ const Page = (): React.ReactNode => {
                         flags.</p>
                 </Tooltip>
             </motion.div>
-            <motion.div variants={container} className={cx("grid grid-cols-6 gap-8 flex-1 auto-rows-min")}>
-                <DataCard variants={container} label={'Target URL'} className={'col-span-full md:col-span-4'}>
+            <motion.div variants={container} className={cx("grid grid-cols-12 gap-8 flex-1 auto-rows-min")}>
+                
+                <DataCard variants={container} label={'Target URL'} className={merge(
+                    'col-span-full',
+                    'md:col-span-8',
+                    'lg:col-span-full',
+                    'xl:col-span-4'
+                )}>
                     <p className={"w-full px-4 py-2 bg-zinc-200 text-zinc-600 rounded-xl border border-transparent font-mono text-wrap break-words overflow-hidden"}>{link.target}</p>
                 </DataCard>
                 
-                <Card contained={false} structured className={'col-start-5 col-span-2 row-span-full'}>
-                
-                </Card>
-                
-                <DataCard variants={container} label={'Click count'} className={'col-span-full sm:col-span-3 md:col-span-2 lg:col-span-1'}>
-                    <P className={cx(`text-${link.clickCount > 0 ? '4' : ''}xl font-bold`)}>
-                        {link.clickCount > 0 ? link.clickCount : "No clicks"}
+                <DataCard variants={container} label={'Click count'} className={merge(
+                    'col-span-full',
+                    'md:col-span-4',
+                    'xl:col-span-2'
+                )}>
+                    <P className={'text-4xl font-bold'}>
+                        {link.clickCount > 0 ? link.clickCount : 'No clicks'}
                     </P>
                 </DataCard>
                 
-                <DataCard variants={container} label={'Flag Count'} className={'col-span-full sm:col-span-3 md:col-span-2 lg:col-span-1'}>
-                    <P className={cx(`text-${link.clickCount > 0 ? '4' : ''}xl font-bold`)}>
-                        {link.flagCount || 'No flags'}
-                    </P>
-                </DataCard>
-                
-                <DataCard variants={container} label={'Created at'} className={'col-span-full sm:col-span-3 md:col-span-2'}>
+                <DataCard variants={container} label={'Created at'} className={merge(
+                    'col-span-full',
+                    'md:col-span-6',
+                    'lg:col-span-4',
+                    'xl:col-span-3'
+                )}>
                     <P className={'text-xl font-bold'}><DateToLocaleString value={link.createdAt}/></P>
-                    <P className={'text-base'}>By {user!.email === link.createdBy.email ? 'you' : link.createdBy.email}</P>
+                    <P className={'text-base'}>By {user!.email === link.createdBy!.email ? 'you' : link.createdBy!.email}</P>
                 </DataCard>
                 
-                <DataCard variants={container} label={'Last modified'} className={'col-span-full sm:col-span-3 md:col-span-2'}>
+                <DataCard variants={container} label={'Last modified'} className={merge(
+                    'col-span-full',
+                    'md:col-span-6',
+                    'lg:col-span-4',
+                    'xl:col-span-3'
+                )}>
                     <P className={'text-xl font-bold'}>{link.version > 0 && link.updatedAt ?
                         <DateToLocaleString value={link.updatedAt}/> : "Not modified yet"}</P>
                     <P className={'text-base'}>Version {link.version + 1}</P>
                 </DataCard>
                 
-                <Card variants={container} contained={false} className={"col-span-full flex-row justify-between"}>
+                <Suspense fallback={<ClickListSkeleton />}>
+                    <ClickList className={merge(
+                        'col-span-full',
+                        'lg:col-span-6',
+                    )} />
+                </Suspense>
+                
+                <FlagList className={merge(
+                    'col-span-full',
+                    'lg:col-span-6',
+                )} />
+                
+                <Card variants={container} contained={false} className={"col-span-full flex-row justify-between mt-auto"}>
                     <Link passHref href={'/dashboard'}><Button btnType={'theme'} variants={item}>Go back</Button></Link>
                     <ButtonGroup className={"justify-end"}>
                         <Link passHref href={`/dashboard/${link.id}/modify`}><Button btnType={'theme'} variants={item}>Modify</Button></Link>
@@ -104,5 +128,3 @@ const Page = (): React.ReactNode => {
         </>
     )
 }
-
-export default Page;
