d32c53c45d0ee2c1700ee4e721433261fa0edd31
Sebastian Wirkijowski <sebastian@wirkijowski.dev>
2025-02-09 22:41:00 +0100

    feat(client): added classname property to spinner component.
    Signed-off-by: Sebastian Wirkijowski <sebastian@wirkijowski.dev>

--- client/src/components/spinner/spinner.tsx
+++ client/src/components/spinner/spinner.tsx
@@ -1,13 +1,16 @@
 "use client";
 
+import {ReactNode} from "react";
 import {merge} from "@lib/merge";
+import {SCompProps} from "@type/common";
 
-export const Spinner = () => {
+export const Spinner = ({className}: SCompProps.TBase): ReactNode => {
     return (
         <span className={merge(
             'spinner animate-spin font-sm',
             'inline-block relative h-[1em] w-[1em] text-[1em] overflow-hidden',
-            'after:content-[""] after:top-0 after:right-0 after:bottom-0 after:left-0 after:absolute after:block after:rounded-full after:border-[0.25em] after:border-transparent after:border-r-[currentColor] after:border-t-[currentColor]'
+            'after:content-[""] after:top-0 after:right-0 after:bottom-0 after:left-0 after:absolute after:block after:rounded-full after:border-[0.25em] after:border-transparent after:border-r-[currentColor] after:border-t-[currentColor]',
+            className
         )}></span>
     )
 }
